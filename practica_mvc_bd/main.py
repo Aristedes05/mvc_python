c892577dbee383fd1d9893e0ecc3107b6beff3ec
Aristedes05 <65907708+Aristedes05@users.noreply.github.com>
2020-11-01 07:20:13 +0100

    iniciando
    se esta iniciando

---
+++ practica_mvc_bd/main.py
@@ -0,0 +1,6 @@
+from controler.personaControl import controlPersona
+
+lista=["Aristedes","flores","20","62456795","M","0011","I.SOFTWARE","3"]
+
+enlace = controlPersona()
+enlace.CrearRegistro(lista)
